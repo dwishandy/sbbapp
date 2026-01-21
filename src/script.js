bc7b4d1d4f531872d637f031edd2d9be01225709
firshandy <firshandydwi@gmail.com>
2026-01-21 19:55:50 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,33 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const html = document.documentElement;
+const iconMoon = document.getElementById("icon-moon");
+const iconSun = document.getElementById("icon-sun");
+
+function updateIcon() {
+  if (html.classList.contains("dark")) {
+    iconMoon.classList.add("hidden");
+    iconSun.classList.remove("hidden");
+  } else {
+    iconMoon.classList.remove("hidden");
+    iconSun.classList.add("hidden");
+  }
+}
+
+console.log(document.documentElement.classList);
+
+toggleBtn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+
+  const theme = html.classList.contains("dark") ? "dark" : "light";
+  localStorage.setItem("theme", theme);
+
+  updateIcon();
+});
+
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
