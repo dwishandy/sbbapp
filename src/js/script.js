ccb9203eb3c7816fca3832b242b1e1b701a27c84
firshandy <firshandydwi@gmail.com>
2026-01-28 18:22:00 +0700

    unfinished project, move to ui debugging phase

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 // dark and light mode
-
 const toggleBtn = document.getElementById("theme-toggle");
 const html = document.documentElement;
 const iconMoon = document.getElementById("icon-moon");
@@ -33,4 +32,25 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// fiter bidang
+const filterButtons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".service-card");
+
+filterButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const filter = btn.dataset.filter;
+
+    filterButtons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    cards.forEach((card) => {
+      if (filter === "all" || card.dataset.category === filter) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+    });
+  });
 });
